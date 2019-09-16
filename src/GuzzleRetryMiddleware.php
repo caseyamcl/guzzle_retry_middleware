ce552b1dd6e5358bd4b5520dc7e78aac25fbca07
Casey McLaughlin <caseyamcl@gmail.com>
2019-09-16 11:22:26 -0400

    Improve type comparison in `deriveTimeoutFromHeader` method

--- src/GuzzleRetryMiddleware.php
+++ src/GuzzleRetryMiddleware.php
@@ -352,7 +352,7 @@ class GuzzleRetryMiddleware
     {
         // The timeout will either be a number or a HTTP-formatted date,
         // or seconds (integer)
-        if (trim($headerValue) === $headerValue) {
+        if (trim($headerValue) === (string) $headerValue) {
             return (float) trim($headerValue);
         } elseif ($date = DateTime::createFromFormat(self::DATE_FORMAT, trim($headerValue))) {
             return (float) $date->format('U') - time();
